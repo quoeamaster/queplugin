7d52b401237a73db4556988ef009ca482e94a89c
quoeamaster <quoeamaster@gmail.com>
2018-08-07 13:25:21 +0800

    2018-08-07a

--- IDiscoveryPlugin.go
+++ IDiscoveryPlugin.go
@@ -21,7 +21,7 @@ package queplugin
 
 type DiscoveryPlugin interface {
     // method to ping for the given url / broker
-    Ping (url string, options map[string]interface{}) (valid bool, err error)
+    Ping (url string, options map[string]interface{}) (valid bool, returnValues map[string]interface{}, err error)
 
     // perform a master broker election; return the master's broker id
     ElectMaster (params map[string]interface{}) (brokerId string, err error)
